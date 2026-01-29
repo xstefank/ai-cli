ac64b3b49e44336645700ca924f803828d727aeb
xstefank <xstefank122@gmail.com>
2026-01-29 11:17:18 +0100

    Update version of LC4J in fs
    Signed-off-by: xstefank <xstefank122@gmail.com>

--- fs/playground/hello_script.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python hello_script.py <name>')
-        sys.exit(1)
-    name = sys.argv[1]
-    print(f'Hello {name}')
